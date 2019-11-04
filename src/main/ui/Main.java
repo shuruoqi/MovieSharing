324b7634c92adba3e1d98c3567c405cc0591dc5c
ChrissyShu <shuruoqi@students.cs.ubc.ca>
2019-11-04 15:31:41 -0800

    9.5

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,13 +1,9 @@
 package ui;
-
-import exception.ReputationException;
-import exception.VipException;
-
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ReputationException, VipException {
+    public static void main(String[] args) throws IOException {
         ListOperation listOperation = new ListOperation();
         listOperation.start();
     }
